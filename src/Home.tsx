fb17ee972307700fb31603d96b24375f5b7eb668
Guilherme Salkovski <guilherme.salkovski@gmail.com>
2023-10-14 15:42:38 -0300

    feat: add simple input load for json files

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,29 @@
+import { useState } from 'react'
+
 export function Home() {
+  const [files, setFiles] = useState('')
+
+  function handleReadJSON(e: any) {
+    const fileReader = new FileReader()
+    fileReader.readAsText(e.target.files[0], 'UTF-8')
+    fileReader.onload = (e) => {
+      console.log('e.target.result', e.target.result)
+      setFiles(e.target.result)
+    }
+  }
+
   return (
     <div>
       <h1>JSON Tree Viewer</h1>
       <p>Simple JSON Viewer that runs completely on-client. No data exchange</p>
-      <button>Load JSON</button>
+      <input
+        type="file"
+        id="file"
+        name="file"
+        accept="application/json"
+        onChange={handleReadJSON}
+      />
+      {'uploaded file content -- ' + files}
     </div>
   )
 }
