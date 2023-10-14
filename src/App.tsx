68246daa4ccb859e4a5e9b5b3d8b59532adfb9be
Guilherme Salkovski <guilherme.salkovski@gmail.com>
2023-10-14 13:25:46 -0300

    feat: add home page

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css'
+import { Home } from './Home'
+
+function App() {
+  return <Home />
+}
+
+export default App
---
+++ src/Home.tsx
@@ -0,0 +1,9 @@
+export function Home() {
+  return (
+    <div>
+      <h1>JSON Tree Viewer</h1>
+      <p>Simple JSON Viewer that runs completely on-client. No data exchange</p>
+      <button>Load JSON</button>
+    </div>
+  )
+}
