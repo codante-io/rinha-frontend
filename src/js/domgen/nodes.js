5fd2ef7e66c311826a51af769c6ab48f0bdb5b35
pliavi <pliavi.rpgm@gmail.com>
2023-11-05 22:40:00 -0300

    Fix: Add margin to array key and colon nodes

--- src/js/domgen/nodes.js
+++ src/js/domgen/nodes.js
@@ -2,7 +2,7 @@ import { createSpanNode } from "./creators.js";
 
 export const openBracketNode = createSpanNode("[", "orange");
 export const closeBracketNode = createSpanNode("]", "orange");
-export const colonNode = createSpanNode(": ", "orange");
+export const colonNode = createSpanNode(":", "colon");
 export const tabNode = createSpanNode("", "tab");
 export const breakNode = createSpanNode("\n", "break");
 export const nullNode = createSpanNode("null", "string");
